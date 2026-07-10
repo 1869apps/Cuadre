62e92a5f18e91dea7a14a1b0744f79583a852827
1869apps <pedrolh@nauta.cu>
2026-07-09 22:05:12 -0400

    App Cuadre

---
+++ sw.js
@@ -0,0 +1,7 @@
+var CACHE_NAME='com-1869apps-cuadre-v1';
+var CACHE_MAX=52428800;
+var TIMEOUT_MS=15000;
+
+self.addEventListener('install',function(e){self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.filter(function(x){return x!==CACHE_NAME}).map(function(x){return caches.delete(x)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(function(cached){var fp=fetch(e.request).then(function(r){if(r&&r.status===200){var cl=r.clone();caches.open(CACHE_NAME).then(function(c){c.put(e.request,cl)})}return r}).catch(function(){return cached||new Response('Offline',{status:503})});return cached||fp}))});
